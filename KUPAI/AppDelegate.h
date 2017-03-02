8a98f749f756d12da77d834484f68fc295fab6a6
杜博 <905517804@qq.com>
2017-03-02 15:15:40 +0800

    Initial Commit

---
+++ KUPAI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KUPAI
+//
+//  Created by 杜博 on 2017/3/2.
+//  Copyright © 2017年 ------DuBo------. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KUPAI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KUPAI
+//
+//  Created by 杜博 on 2017/3/2.
+//  Copyright © 2017年 ------DuBo------. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
